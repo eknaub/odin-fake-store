606895f31a0fd4767f719460b7656763f1af3eca
knaub <eknaub404@gmail.com>
2025-04-18 21:11:11 +0200

    fix: extract to own file

--- src/app/_context/CartProvider.tsx
+++ src/app/_context/CartProvider.tsx
@@ -1,42 +1,12 @@
 import { createContext, useContext, useReducer } from "react";
 import { CartItem, Product } from "../_api/apiInterfaces";
+import { cartReducer } from "./cartReducer";
 
 interface CartContextType {
   cart: CartItem[];
   addToCart: (product: Product) => void;
   removeFromCart: (productId: number) => void;
   updateQuantity: (productId: number, quantity: number) => void;
-}
-
-type CartAction =
-  | { type: "ADD_TO_CART"; product: Product }
-  | { type: "REMOVE_FROM_CART"; productId: number }
-  | { type: "UPDATE_QUANTITY"; productId: number; quantity: number };
-
-function cartReducer(state: CartItem[], action: CartAction): CartItem[] {
-  switch (action.type) {
-    case "ADD_TO_CART":
-      const existingItem = state.find((item) => item.id === action.product.id);
-      if (existingItem) {
-        return state.map((item) =>
-          item.id === action.product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      } else {
-        return [...state, { ...action.product, quantity: 1 }];
-      }
-    case "REMOVE_FROM_CART":
-      return state.filter((item) => item.id !== action.productId);
-    case "UPDATE_QUANTITY":
-      return state.map((item) =>
-        item.id === action.productId
-          ? { ...item, quantity: action.quantity }
-          : item
-      );
-    default:
-      throw new Error(`Unhandled action type: ${(action as CartAction).type}`);
-  }
 }
 
 const CartContext = createContext<CartContextType | null>(null);
